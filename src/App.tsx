0eea21515cf741d3c8e1f2b5b54cfa5e61164b3b
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-12-07 00:16:19 +0900

    Update: Hello Worldが画面中央に表示されるよう修正

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+const SDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+function App() {
+  return (
+    <>
+      <SDiv>Hello World</SDiv>
+    </>
+  )
+}
+
+export default App
